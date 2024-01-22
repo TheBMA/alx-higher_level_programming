9dfdbcc95607c2d2278eed1795220f8c0c417aaf
TheBMA <benrochd.m.ali@gmail.com>
2024-01-22 01:38:59 -0800

    Pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 10):
     for j in range(i, 10):
         if (i != j) and (i*j != 72):
-            print("{:d}{:d}".format(i, j), end= ", ")
+            print("{:d}{:d}".format(i, j), end=", ")
 print("89")
