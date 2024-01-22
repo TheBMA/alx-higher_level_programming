a69b3833f0e775490fcf30070bfe74bf0c4364c8
TheBMA <benrochd.m.ali@gmail.com>
2024-01-22 12:34:56 -0800

    requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("1 argument:")
-    print("1: {:s}".format(argv[1]))
-else:
-    print("{:d} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{:d}: {:s}".format(i, argv[i]))
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
