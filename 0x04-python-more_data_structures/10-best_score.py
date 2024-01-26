a1edabb6aa47ec8154e183cf6dfb37f845bf3035
TheBMA <benrochd.m.ali@gmail.com>
2024-01-26 02:25:07 -0800

    handles if a_dictionary is empty or None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if not a_dictionary:
         best_value = 0
         for key in a_dictionary:
             if a_dictionary[key] > best_value:
