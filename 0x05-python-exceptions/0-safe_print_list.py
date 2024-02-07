dc98761ae059a5a9b43b9497bc580f189dbe9679
TheBMA <benrochd.m.ali@gmail.com>
2024-02-07 09:23:41 -0800

    Check for empty list and x=0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-        print()
-        return i + 1
+        if x > 0 and my_list:
+            for i in range(x):
+                print(my_list[i], end="")
+            print()
+            return i + 1
     except IndexError:
         print()
-        return i
+    return i
