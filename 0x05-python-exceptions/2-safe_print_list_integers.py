921754a1a9c7040857ae98e21b4ee52e35766122
TheBMA <benrochd.m.ali@gmail.com>
2024-02-07 12:54:17 -0800

    added check for empty list and x=0

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,16 +3,16 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     j = 0
     try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-            else:
-                j += 1
-                continue
+        if x > 0 and my_list:
+            for i in range(x):
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end="")
+                else:
+                    j += 1
+                    continue
+            i += 1
     except (ValueError, TypeError):
         j += 1
         pass
-    else:
-        i += 1
     print()
     return i - j
