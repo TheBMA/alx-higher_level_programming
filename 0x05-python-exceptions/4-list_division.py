e8b58b01dd0cad0b66d589f2b039dfc526f89cdb
TheBMA <benrochd.m.ali@gmail.com>
2024-02-07 14:29:54 -0800

    in case one list is shorter

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    i = 0
     new_list = []
-    if list_length > 0 and my_list_1 and my_list_2:
-        for i in range(list_length):
-            try:
-                new_list.append(my_list_1[i] / my_list_2[i])
-            except ZeroDivisionError:
-                print("division by 0")
-                new_list.append(0)
-            except TypeError:
-                print("wrong type")
-                new_list.append(0)
-            except IndexError:
-                print("out of range")
-                new_list.append(0)
-            finally:
-                continue
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
 
     return new_list
